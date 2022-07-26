dfb3b0d9e64d6b3468a5568c8cf5d5db983827b1
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-07-26 18:03:46 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,37 @@ public class Main {
         mentoria1.setDescricao("Descrição Mentoria Java");
         mentoria1.setDataDaMentoria(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Dercrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscrever(bootcamp);
+        System.out.println("Conteúdos Inscritos Pedro" + devPedro.getConteudosInscritos());
+        devPedro.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos Pedro" + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Pedro" + devPedro.getConteudosConcluidos());
+        System.out.println("XP: " + devPedro.calcularTotalXp());
+
+        System.out.println("-----------------");
+
+        Dev devGustavo = new Dev();
+        devGustavo.setNome("Gustavo");
+        devGustavo.inscrever(bootcamp);
+        System.out.println("Conteúdos Inscritos Gustavo" + devGustavo.getConteudosInscritos());
+        devGustavo.progredir();
+        System.out.println("--");
+        System.out.println("XP: " + devPedro.calcularTotalXp());
+        System.out.println("Conteúdos Inscritos Gustavo" + devGustavo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Gustavo" + devGustavo.getConteudosConcluidos());
     }
 }
